8493cc5d2f31329d11187d5255996144a0f7f557
jwilliam96 <jwsa.1996@gmail.com>
2024-09-28 23:09:14 -0500

    correo

---
+++ src/action/secret.ts
@@ -0,0 +1,14 @@
+"use server"
+
+export const getSecret = () => {
+    const serviceId = process.env.EMAILJS_SERVICE_ID;
+    const templateId = process.env.EMAILJS_TEMPLATE_ID;
+    const publicKey = process.env.EMAILJS_PUBLIC_KEY;
+
+
+    if (!serviceId || !templateId || !publicKey) {
+        throw new Error("Las claves de entorno no están definidas");
+    }
+
+    return { serviceId, templateId, publicKey }
+}
--- src/ui/contactanos/Form.tsx
+++ src/ui/contactanos/Form.tsx
@@ -1,40 +1,61 @@
-"use client"
+"use client";
 
-import { useForm } from "react-hook-form"
-
+import { useForm } from "react-hook-form";
+import emailjs from "@emailjs/browser";
+import Swal from "sweetalert2";
+import { useRef } from "react";
+import { getSecret } from "@/action/secret";
 
 interface FormData {
-    name: string
-    email: string
-    tel: string
-    plans: "Futuro seguro" | "Impulsa tu futuro" | "Elige un plan"
-    call: string
+    name: string;
+    email: string;
+    tel: string;
+    plans: "Futuro seguro" | "Impulsa tu futuro" | "Elige un plan";
+    call: string;
 }
 
 export default function Form() {
+    const form = useRef<HTMLFormElement>(null);
+    const { register, handleSubmit, reset, formState: { errors } } = useForm<FormData>();
 
-    const { register, handleSubmit, reset, formState: { errors } } = useForm<FormData>()
 
 
-    const submit = (data: FormData) => {
 
-        reset({
-            name: "",
-            call: "",
-            email: "",
-            plans: "Elige un plan",
-            tel: ""
-        })
+    const submit = async () => {
 
-    }
+        const secret = await getSecret()
+
+        emailjs
+            .sendForm(secret.serviceId, secret.templateId, form.current!, secret.publicKey)
+            .then(
+                () => {
+                    Swal.fire({
+                        position: "center",
+                        icon: "success",
+                        title: "Mensaje enviado",
+                        showConfirmButton: false,
+                        timer: 1000,
+                    });
+
+                    reset({
+                        name: "",
+                        call: "",
+                        email: "",
+                        plans: "Elige un plan",
+                        tel: ""
+                    });
+                },
+                (error) => {
+                    console.log("FAILED...", error.text);
+                }
+            );
+    };
 
     return (
-        <form onSubmit={handleSubmit(submit)} className=" flex justify-center items-center flex-grow pt-10">
+        <form ref={form} onSubmit={handleSubmit(submit)} className=" flex justify-center items-center flex-grow pt-10">
             <div className="max-w-[950px] grid md:grid-cols-2 gap-8 md:gap-x-16 w-full">
-
                 <div className="flex flex-col gap-2 ">
                     <label htmlFor="name" className="text-2xl">Nombre Completo</label>
-
                     <div className="relative p-1 bg-gradient-to-r from-purple-600 to-pink-400 rounded-2xl">
                         <input
                             id="name"
@@ -45,7 +66,6 @@ export default function Form() {
                         />
                     </div>
                     {errors.name && <p className="text-red-500 text-xl pl-6">* Campo requerido</p>}
-
                 </div>
 
                 <div className="flex flex-col gap-2 ">
@@ -79,37 +99,16 @@ export default function Form() {
                 <div className="flex flex-col gap-2 ">
                     <p className="text-2xl">Plan de contacto</p>
                     <div className="relative p-1 bg-gradient-to-r from-purple-600 to-pink-400 rounded-2xl">
-                        {/* <input
-                            id="plans"
-                            {...register("plans")}
-                            type="text"
-                            className="w-full p-4 rounded-2xl bg-white border-none outline-none text-black"
-                            placeholder="Nombre"
-                        /> */}
                         <select
-                            name="plan"
-                            id="plan"
+                            {...register("plans")}
                             className="w-full p-4 rounded-2xl bg-white border-none outline-none text-gray-700 cursor-pointer"
                         >
-                            <option value="" className="text-gray-400">Elige un plan</option>
-                            <option value="">Futuro seguro</option>
-                            <option value="">Impulsa tu futuro</option>
+                            <option value="Elige un plan" className="text-gray-400">Elige un plan</option>
+                            <option value="Futuro seguro">Futuro seguro</option>
+                            <option value="Impulsa tu futuro">Impulsa tu futuro</option>
                         </select>
                     </div>
                 </div>
-
-                {/* <div className="flex flex-col gap-2 ">
-                    <label htmlFor="cc" className="text-2xl">N° de Identificación</label>
-                    <div className="relative p-1 bg-gradient-to-r from-purple-600 to-pink-400 rounded-2xl">
-                        <input
-                            id="cc"
-                            {...register("cc")}
-                            type="text"
-                            className="w-full p-4 rounded-2xl bg-white border-none outline-none text-black"
-                            placeholder="Nombre"
-                        />
-                    </div>
-                </div> */}
 
                 <div className="flex flex-col gap-2 ">
                     <label htmlFor="call" className="text-2xl">Horario de llamada</label>
@@ -123,9 +122,11 @@ export default function Form() {
                         />
                     </div>
                 </div>
-                <button className={`bg-gradient-to-b from-custom-purple to-custom-pink text-white text-xl font-bold rounded-full px-12 py-3 md:col-span-2 max-w-max justify-self-center`}>Enviar</button>
-            </div>
 
+                <button className="bg-gradient-to-b from-custom-purple to-custom-pink text-white text-xl font-bold rounded-full px-12 py-3 md:col-span-2 max-w-max justify-self-center">
+                    Enviar
+                </button>
+            </div>
         </form>
-    )
+    );
 }
